5ec3877e5b623f692d0b4af14a71628fbeffec82
kamtoeddy <lollipopkalog@gmail.com>
2022-07-25 05:13:11 +0100

    setup tests

---
+++ src/_tests_/schema/index.test.ts
@@ -0,0 +1,30 @@
+import { UserModel } from ".";
+import { IUser } from "./interface";
+
+describe("Testing User Schema", () => {
+  let user: IUser;
+
+  beforeAll(async () => {
+    user = await UserModel({
+      id: "1",
+      dob: "yesterday",
+      firstName: " John",
+      lastName: "Smith ",
+    }).create();
+
+    console.log(user);
+  });
+
+  it("should initialise with properties assigned", () => {
+    expect(user).toMatchObject<IUser>({
+      id: "1",
+      dob: "yesterday",
+      firstName: "John",
+      lastName: "Smith",
+    });
+  });
+
+  it("should initialise and set values of dependent properties", () => {
+    expect(user).toMatchObject<IUser>({ name: "John Smith" });
+  });
+});
