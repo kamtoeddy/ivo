25f343adce1fa33c87eff2eef7557b545e986c59
kamtoeddy <lollipopkalog@gmail.com>
2022-08-01 23:10:24 +0100

    trying to fix ci error

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { ApiError } from "./utils/ApiError.js";
 export { isEqual } from "./utils/isEqual.js";
-export { makeModel } from "./schema/model";
+export { Model, makeModel } from "./schema/model";
 export { Schema } from "./schema";
 export * as validate from "./validate";
