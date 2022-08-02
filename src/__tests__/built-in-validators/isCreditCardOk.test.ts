6c051c343c9526f66cb781d7657e87d4e6648c62
kamtoeddy <lollipopkalog@gmail.com>
2022-08-02 12:57:44 +0100

    added built-in validator: isCreditCardOk + its tests

---
+++ src/__tests__/built-in-validators/isCreditCardOk.test.ts
@@ -0,0 +1,34 @@
+import { isCreditCardOk } from "../../validate/isCreditCardOk";
+
+describe("Testing isCreditCardOk", () => {
+  it("should tell whether a character is a valid credit card number", () => {
+    const truthyvalues = [5420596721435293, "5420596721435293"];
+
+    truthyvalues.forEach((value) => {
+      expect(isCreditCardOk(value)).toMatchObject({
+        reasons: [],
+        valid: true,
+        validated: value,
+      });
+    });
+
+    // falsy tests
+    const falsyValues = [
+      -1000,
+      0,
+      1,
+      "",
+      "123-2342-25-6750",
+      4187622910505690,
+      "4187622910505690",
+    ];
+
+    falsyValues.forEach((value) => {
+      expect(isCreditCardOk(value)).toMatchObject({
+        reasons: ["Invalid credit card"],
+        valid: false,
+        validated: undefined,
+      });
+    });
+  });
+});
