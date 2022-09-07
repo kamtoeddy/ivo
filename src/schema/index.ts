423fc80c68412ee26f1f286262abb6377eadade0
kamtoeddy <lollipopkalog@gmail.com>
2022-09-07 12:53:53 +0100

    more cleanups

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -3,6 +3,12 @@ import { ObjectType } from "../utils/interfaces";
 import { Schema as ns, StringKey } from "./interfaces";
 import { defaultOptions, SchemaCore } from "./SchemaCore";
 
+interface Model<T> {
+  clone: (options?: ns.CloneOptions<T>) => Promise<T>;
+  create: () => Promise<T>;
+  update: (changes: Partial<T>) => Promise<Partial<T>>;
+}
+
 export class Schema<T extends ObjectType> extends SchemaCore<T> {
   constructor(
     propDefinitions: ns.PropertyDefinitions<T>,
@@ -41,22 +47,27 @@ export class Schema<T extends ObjectType> extends SchemaCore<T> {
     return this._useExtensionOptions(options);
   };
 
-  getModel = () => Model.build(this);
+  getModel = () => {
+    const model = new ModelTool(this);
+
+    return (values: Partial<T>): Model<T> => {
+      model.setValues(values);
+
+      return {
+        clone: (options?: ns.CloneOptions<T>) => model.clone(options),
+        create: () => model.create(),
+        update: (changes: Partial<T>) => model.update(changes),
+      };
+    };
+  };
 }
 
-class Model<T extends ObjectType> extends SchemaCore<T> {
-  constructor(schema: Schema<T>, values: Partial<T>) {
+class ModelTool<T extends ObjectType> extends SchemaCore<T> {
+  constructor(schema: Schema<T>) {
     super(schema.propDefinitions, schema.options);
-    this.setValues(values);
   }
 
-  static build<U extends ObjectType>(schema: Schema<U>) {
-    return function Builder(values: Partial<U>): Model<U> {
-      return new Model(schema, values);
-    };
-  }
-
-  private setValues(values: Partial<T>) {
+  setValues(values: Partial<T>) {
     const keys = Object.keys(values).filter(
       (key) =>
         this._helper.isTimestampKey(key) ||
@@ -64,8 +75,9 @@ class Model<T extends ObjectType> extends SchemaCore<T> {
         this._isSideEffect(key)
     ) as StringKey<T>[];
 
-    sort(keys).forEach((key) => (this.values[key] = values[key]));
+    this.values = {};
 
+    sort(keys).forEach((key) => (this.values[key] = values[key]));
     this._initContext();
   }
 
@@ -102,117 +114,3 @@ class Model<T extends ObjectType> extends SchemaCore<T> {
     return this._useConfigProps(updated, true);
   };
 }
-
-// interface Model<T> {
-//   clone: (options?: ns.CloneOptions<T>) => Promise<T>;
-//   create: () => Promise<T>;
-//   update: (changes: Partial<T>) => Promise<Partial<T>>;
-// }
-
-// export class Schema<T extends ObjectType> extends SchemaCore<T> {
-//   constructor(
-//     propDefinitions: ns.PropertyDefinitions<T>,
-//     options: ns.Options = defaultOptions
-//   ) {
-//     super(propDefinitions, options);
-//   }
-
-//   get options() {
-//     return this._options;
-//   }
-
-//   get propDefinitions() {
-//     return this._propDefinitions;
-//   }
-
-//   private _useExtensionOptions = <T extends ObjectType>(
-//     options: ns.ExtensionOptions<T>
-//   ) => {
-//     const remove = toArray(options?.remove ?? []);
-
-//     remove?.forEach((prop) => delete this._propDefinitions?.[prop]);
-
-//     return this;
-//   };
-
-//   extend = <U extends ObjectType>(
-//     parent: Schema<U>,
-//     options: ns.ExtensionOptions<U> = { remove: [] }
-//   ) => {
-//     this._propDefinitions = {
-//       ...parent.propDefinitions,
-//       ...this._propDefinitions,
-//     } as ns.PropertyDefinitions<T>;
-
-//     return this._useExtensionOptions(options);
-//   };
-
-//   getModel = () => {
-//     const model = new ModelTool(this);
-
-//     return (values: Partial<T>) => {
-//       model.setValues(values);
-
-//       return {
-//         clone: (options?: ns.CloneOptions<T>) => model.clone(options),
-
-//         create: () => model.create(),
-
-//         update: (changes: Partial<T>) => model.update(changes),
-//       } as Model<T>;
-//     };
-//   };
-// }
-
-// class ModelTool<T extends ObjectType> extends SchemaCore<T> {
-//   constructor(schema: Schema<T>) {
-//     super(schema.propDefinitions, schema.options);
-//   }
-
-//   setValues(values: Partial<T>) {
-//     const keys = Object.keys(values).filter(
-//       (key) =>
-//         this._helper.isTimestampKey(key) ||
-//         this._isProp(key) ||
-//         this._isSideEffect(key)
-//     ) as StringKey<T>[];
-
-//     this.values = {};
-
-//     sort(keys).forEach((key) => (this.values[key] = values[key]));
-//     this._initContext();
-//   }
-
-//   clone = async (options: ns.CloneOptions<T> = { reset: [] }) => {
-//     return this._getCloneObject(toArray(options.reset).filter(this._isProp));
-//   };
-
-//   create = async () => this._getCreateObject();
-
-//   update = async (changes: Partial<T>) => {
-//     const updated = {};
-
-//     const toUpdate = Object.keys(changes ?? {}).filter((prop) =>
-//       this._isUpdatable(prop)
-//     ) as StringKey<T>[];
-
-//     const validations = toUpdate.map((prop) => {
-//       return this._validateAndSet(updated, prop, changes[prop]);
-//     });
-
-//     await Promise.all(validations);
-
-//     if (this._isErroneous()) this._throwError();
-
-//     const linkedProps = toUpdate.filter((prop) => !this._isSideEffect(prop));
-//     const sideEffects = toUpdate.filter(this._isSideEffect);
-
-//     await this._resolveLinked(linkedProps, updated, "onUpdate");
-
-//     await this._resolveLinked(sideEffects, updated, "onUpdate");
-
-//     if (!Object.keys(updated).length) this._throwError("Nothing to update");
-
-//     return this._useConfigProps(updated, true);
-//   };
-// }
