c2c8971e9bbc971b809282fc924e073a76005f02
kamtoeddy <lollipopkalog@gmail.com>
2023-01-05 20:51:36 +0100

    fix: fixed problem of context of operation of onSuccess listeners missing some values

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -155,9 +155,10 @@ class ModelTool<T extends ObjectType> extends SchemaCore<T> {
     data: Partial<T>,
     lifeCycle: LifeCycles.LifeCycle
   ) => {
-    const ctx = this._getFinalContext();
+    const ctx = this._getContext();
+    const finalCtx = this._getFinalContext();
 
-    const successFulSideEffects = this._getKeysAsProps(ctx).filter(
+    const successFulSideEffects = this._getKeysAsProps(finalCtx).filter(
       this._isSideEffect
     );
 
