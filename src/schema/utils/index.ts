431c37bd27f468681b0dc596d678eed3026f7887
kamtoeddy <lollipopkalog@gmail.com>
2023-01-21 22:51:37 +0100

    feat: removed reasons from ValidationResponse when validation is successful

--- src/schema/utils/index.ts
+++ src/schema/utils/index.ts
@@ -7,7 +7,7 @@ export const makeResponse = <T = undefined>(
   if (input.valid) {
     const { valid, validated } = input;
 
-    return { reasons: [], valid, validated } as ValidatorResponse<TypeOf<T>>;
+    return { valid, validated } as ValidatorResponse<TypeOf<T>>;
   }
 
   let { reason, reasons, valid } = input as any;
