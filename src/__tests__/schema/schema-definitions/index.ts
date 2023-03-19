ee6baa3c1f456d0fb6a089918d2a5f4f8e35cb06
kamtoeddy <kamtoeddytagne@gmail.com>
2023-03-19 14:43:11 +0100

    test: added test to make sure that errors related to aliases returned during cloning are returned with the alias name on payload error

--- src/__tests__/schema/schema-definitions/index.ts
+++ src/__tests__/schema/schema-definitions/index.ts
@@ -3169,14 +3169,18 @@ export const schemaDefinition_Tests = ({ Schema }: any) => {
                 expect(contextRecord).toEqual({});
               });
 
-              // it("should return alias errors with alias name in error payload at creation", async () => {
-              //   const { error } = await Model.create({ qty: "12" });
+              it("should return alias errors with alias name in error payload during cloning", async () => {
+                const { error } = await Model.create({
+                  id: 1,
+                  quantity: 2000,
+                  qty: "5",
+                });
 
-              //   expect(error.payload).toMatchObject({
-              //     qty: ["Invalid quantity"],
-              //   });
-              //   expect(contextRecord).toEqual({});
-              // });
+                expect(error.payload).toMatchObject({
+                  qty: ["Invalid quantity"],
+                });
+                expect(contextRecord).toEqual({});
+              });
 
               // it("should respect precedence of virtual property and alias if both are provided at creation", async () => {
               //   const operation1 = await Model.create({
