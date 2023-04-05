34946d3250e5c41a78199eae959a59bb6880b288
kamtoeddy <kamtoeddytagne@gmail.com>
2023-04-05 09:57:43 +0100

    test: added test to make sure that 'onSuccess' option of schema only accepts a function or an array of functions

--- src/__tests__/schema/schema-definitions/options/on-success.ts
+++ src/__tests__/schema/schema-definitions/options/on-success.ts
@@ -20,5 +20,41 @@ export const Test_SchemaOnSuccess = ({ Schema, fx }: any) => {
         }
       });
     });
+
+    describe("invalid", () => {
+      it("should reject 'onSuccess' other than (() => any) | ((() => any)[])", () => {
+        const invalidValues = [
+          1,
+          0,
+          -14,
+          true,
+          false,
+          "invalid",
+          "",
+          null,
+          undefined,
+        ];
+
+        for (const onSuccess of invalidValues) {
+          const toFail = fx(getValidSchema(), { onSuccess });
+
+          expectFailure(toFail);
+
+          try {
+            toFail();
+          } catch (err: any) {
+            expect(err).toMatchObject({
+              message: "Invalid Schema",
+              payload: {
+                onSuccess: expect.arrayContaining([
+                  "The success listener @[0] is not a function",
+                ]),
+              },
+              statusCode: 500,
+            });
+          }
+        }
+      });
+    });
   });
 };
