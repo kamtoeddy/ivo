5aa84d954481d2f55f38069f74a3a4cbde01bf69
kamtoeddy <kamtoeddytagne@gmail.com>
2023-04-05 11:02:00 +0100

    test: added test to make sure global success listeners get called at creation

--- src/__tests__/schema/schema-definitions/options/on-success.ts
+++ src/__tests__/schema/schema-definitions/options/on-success.ts
@@ -1,12 +1,72 @@
 import {
   expectFailure,
   expectNoFailure,
-  expectPromiseFailure,
   getValidSchema,
+  validator,
 } from "../_utils";
 
 export const Test_SchemaOnSuccess = ({ Schema, fx }: any) => {
   describe("Schema.options.onSuccess", () => {
+    describe("behaviour", () => {
+      let successValues: any = {};
+
+      function onSuccess_(prop = "") {
+        return (summary: any) => (successValues[prop] = summary);
+      }
+
+      beforeEach(() => {
+        successValues = {};
+      });
+
+      describe("behaviour with other success listeners", () => {
+        const Book = new Schema(
+          {
+            id: { constant: true, value: 1, onSuccess: onSuccess_("id") },
+            name: { required: true, validator, onSuccess: onSuccess_("name") },
+            price: {
+              default: null,
+              dependent: true,
+              dependsOn: "_setPrice",
+              resolver: ({ _setPrice }: any) => _setPrice,
+              onSuccess: onSuccess_("price"),
+            },
+            _setPrice: {
+              virtual: true,
+              validator,
+              onSuccess: onSuccess_("_setPrice"),
+            },
+          },
+          { onSuccess: onSuccess_("global") }
+        ).getModel();
+
+        it("should trigger all 'success' listeners at creation", async () => {
+          const { data, handleSuccess } = await Book.create({
+            name: "Book name",
+            _setPrice: 100,
+          });
+
+          await handleSuccess();
+
+          const values = { id: 1, name: "Book name", price: 100 };
+          const summary = {
+            context: { ...values, _setPrice: 100 },
+            operation: "creation",
+            previousValues: undefined,
+            values: values,
+          };
+
+          expect(data).toEqual(values);
+          expect(successValues).toEqual({
+            id: summary,
+            name: summary,
+            price: summary,
+            _setPrice: summary,
+            global: summary,
+          });
+        });
+      });
+    });
+
     describe("valid", () => {
       it("should allow 'onSuccess' as (() => any) | ((() => any)[])", () => {
         const values = [() => {}, [() => {}]];
