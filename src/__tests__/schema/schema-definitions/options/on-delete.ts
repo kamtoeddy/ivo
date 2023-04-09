a79d6d9e5a53fa667fc868270ff0b4f28faa09aa
kamtoeddy <kamtoeddytagne@gmail.com>
2023-04-09 14:04:21 +0100

    test: added tests to make sure global onDelete handlers get called with correct values during deletion

--- src/__tests__/schema/schema-definitions/options/on-delete.ts
+++ src/__tests__/schema/schema-definitions/options/on-delete.ts
@@ -7,210 +7,74 @@ import {
 
 export const Test_SchemaOnDelete = ({ Schema, fx }: any) => {
   describe("Schema.options.onDelete", () => {
-    // describe("behaviour", () => {
-    //   let successValues: any = {};
+    describe("behaviour", () => {
+      const values = { id: 1, name: "Book name", price: 100 };
+      let deletedValues: any = {};
 
-    //   function onDelete_(prop = "") {
-    //     return (summary: any) => (successValues[prop] = summary);
-    //   }
+      function onDelete_(prop = "") {
+        return (values: any) => (deletedValues[prop] = values);
+      }
 
-    //   beforeEach(() => {
-    //     successValues = {};
-    //   });
+      beforeEach(() => (deletedValues = {}));
 
-    //   describe("behaviour with other success listeners", () => {
-    //     const Book = new Schema(
-    //       {
-    //         id: { constant: true, value: 1, onDelete: onDelete_("id") },
-    //         name: { required: true, validator, onDelete: onDelete_("name") },
-    //         price: {
-    //           default: null,
-    //           dependent: true,
-    //           dependsOn: "_setPrice",
-    //           resolver: ({ context }: any) => context._setPrice,
-    //           onDelete: onDelete_("price"),
-    //         },
-    //         _setPrice: {
-    //           virtual: true,
-    //           validator,
-    //           onDelete: onDelete_("_setPrice"),
-    //         },
-    //       },
-    //       { onDelete: onDelete_("global") }
-    //     ).getModel();
+      describe("behaviour with other delete handlers", () => {
+        const Book = new Schema(
+          {
+            id: { constant: true, value: 1, onDelete: onDelete_("id") },
+            name: { required: true, validator, onDelete: onDelete_("name") },
+            price: {
+              default: null,
+              dependent: true,
+              dependsOn: "_setPrice",
+              resolver: ({ context }: any) => context._setPrice,
+              onDelete: onDelete_("price"),
+            },
+            _setPrice: {
+              virtual: true,
+              validator,
+            },
+          },
+          { onDelete: onDelete_("global") }
+        ).getModel();
 
-    //     it("should trigger all 'success' listeners at creation", async () => {
-    //       const { data, handleSuccess } = await Book.create({
-    //         name: "Book name",
-    //         _setPrice: 100,
-    //       });
+        it("should trigger all 'delete' handlers on properties an global handlers", async () => {
+          await Book.delete(values);
 
-    //       await handleSuccess();
+          expect(deletedValues).toEqual({
+            id: values,
+            name: values,
+            price: values,
+            global: values,
+          });
+        });
+      });
 
-    //       const values = { id: 1, name: "Book name", price: 100 };
-    //       const summary = {
-    //         context: { ...values, _setPrice: 100 },
-    //         operation: "creation",
-    //         previousValues: undefined,
-    //         values: values,
-    //       };
+      describe("behaviour without other delete handlers", () => {
+        const Book = new Schema(
+          {
+            id: { constant: true, value: 1 },
+            name: { required: true, validator },
+            price: {
+              default: null,
+              dependent: true,
+              dependsOn: "_setPrice",
+              resolver: ({ context }: any) => context._setPrice,
+            },
+            _setPrice: {
+              virtual: true,
+              validator,
+            },
+          },
+          { onDelete: [onDelete_("global"), onDelete_("global-1")] }
+        ).getModel();
 
-    //       expect(data).toEqual(values);
-    //       expect(successValues).toEqual({
-    //         id: summary,
-    //         name: summary,
-    //         price: summary,
-    //         _setPrice: summary,
-    //         global: summary,
-    //       });
-    //     });
+        it("should trigger all global 'delete' handlers", async () => {
+          await Book.delete(values);
 
-    //     it("should trigger all 'success' listeners during cloning ", async () => {
-    //       const book = { id: 1, name: "Book name", price: 100 };
-
-    //       const { data, handleSuccess } = await Book.clone({
-    //         ...book,
-    //         _setPrice: 100,
-    //       });
-
-    //       await handleSuccess();
-
-    //       const summary = {
-    //         context: { ...book, _setPrice: 100 },
-    //         operation: "creation",
-    //         previousValues: undefined,
-    //         values: book,
-    //       };
-
-    //       expect(data).toEqual(book);
-    //       expect(successValues).toEqual({
-    //         id: summary,
-    //         name: summary,
-    //         price: summary,
-    //         _setPrice: summary,
-    //         global: summary,
-    //       });
-    //     });
-
-    //     it("should trigger all 'success' listeners during updates ", async () => {
-    //       const book = { id: 1, name: "Book name", price: 100 };
-
-    //       const { data, handleSuccess } = await Book.update(book, {
-    //         _setPrice: 200,
-    //       });
-
-    //       await handleSuccess();
-
-    //       const values = { ...book, price: 200 };
-
-    //       const summary = {
-    //         context: { ...values, _setPrice: 200 },
-    //         operation: "update",
-    //         previousValues: book,
-    //         values: values,
-    //       };
-
-    //       expect(data).toEqual({ price: 200 });
-    //       expect(successValues).toEqual({
-    //         price: summary,
-    //         _setPrice: summary,
-    //         global: summary,
-    //       });
-    //     });
-    //   });
-
-    //   describe("behaviour without other success listeners", () => {
-    //     const Book = new Schema(
-    //       {
-    //         id: { constant: true, value: 1 },
-    //         name: { required: true, validator },
-    //         price: {
-    //           default: null,
-    //           dependent: true,
-    //           dependsOn: "_setPrice",
-    //           resolver: ({ context }: any) => context._setPrice,
-    //         },
-    //         _setPrice: {
-    //           virtual: true,
-    //           validator,
-    //         },
-    //       },
-    //       { onDelete: [onDelete_("global"), onDelete_("global-1")] }
-    //     ).getModel();
-
-    //     it("should trigger all 'success' listeners at creation", async () => {
-    //       const { data, handleSuccess } = await Book.create({
-    //         name: "Book name",
-    //         _setPrice: 100,
-    //       });
-
-    //       await handleSuccess();
-
-    //       const values = { id: 1, name: "Book name", price: 100 };
-    //       const summary = {
-    //         context: { ...values, _setPrice: 100 },
-    //         operation: "creation",
-    //         previousValues: undefined,
-    //         values: values,
-    //       };
-
-    //       expect(data).toEqual(values);
-    //       expect(successValues).toEqual({
-    //         global: summary,
-    //         "global-1": summary,
-    //       });
-    //     });
-
-    //     it("should trigger all 'success' listeners during cloning ", async () => {
-    //       const book = { id: 1, name: "Book name", price: 100 };
-
-    //       const { data, handleSuccess } = await Book.clone({
-    //         ...book,
-    //         _setPrice: 100,
-    //       });
-
-    //       await handleSuccess();
-
-    //       const summary = {
-    //         context: { ...book, _setPrice: 100 },
-    //         operation: "creation",
-    //         previousValues: undefined,
-    //         values: book,
-    //       };
-
-    //       expect(data).toEqual(book);
-    //       expect(successValues).toEqual({
-    //         global: summary,
-    //         "global-1": summary,
-    //       });
-    //     });
-
-    //     it("should trigger all 'success' listeners during updates ", async () => {
-    //       const book = { id: 1, name: "Book name", price: 100 };
-
-    //       const { data, handleSuccess } = await Book.update(book, {
-    //         _setPrice: 200,
-    //       });
-
-    //       await handleSuccess();
-
-    //       const values = { ...book, price: 200 };
-
-    //       const summary = {
-    //         context: { ...values, _setPrice: 200 },
-    //         operation: "update",
-    //         previousValues: book,
-    //         values: values,
-    //       };
-
-    //       expect(data).toEqual({ price: 200 });
-    //       expect(successValues).toEqual({
-    //         global: summary,
-    //         "global-1": summary,
-    //       });
-    //     });
-    //   });
-    // });
+          expect(deletedValues).toEqual({ global: values, "global-1": values });
+        });
+      });
+    });
 
     describe("valid", () => {
       it("should allow 'onDelete' as (() => any) | ((() => any)[])", () => {
