ed92a88139905422e65dee63d23c114bde375587
kamtoeddy <kamtoeddytagne@gmail.com>
2023-08-22 22:23:58 +0100

    chore: add TODO.js file

---
+++ src/TODO.ts
@@ -0,0 +1,28 @@
+import { Schema } from './schema'
+
+// TODO write tests to make sure this logic is respected
+
+const data = {
+    _id: 'FP4LK2H4LKV',
+    accountStatus: 'active',
+    name: 'Admin',
+    role: 'admin',
+    __typename: 'User'
+  },
+  update = {
+    __typename: 'User',
+    _id: 'FP4LK2H4LKV',
+    accountStatus: 'active',
+    name: 'Admin',
+    role: 'admin'
+  }
+
+const value = { prop: data }
+
+const Model = new Schema({
+  prop: { default: data }
+}).getModel()
+
+Model.update(value, { prop: data }).then(({ data }) => {
+  console.log(data)
+})
