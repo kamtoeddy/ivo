58a6478b15cf3a496c46cd32c3434a55b2b37c65
kamtoeddy <kamtoeddytagne@gmail.com>
2023-10-12 00:16:52 +0100

    chore: add test coverage dependency

--- src/schema/utils/schema-error.ts
+++ src/schema/utils/schema-error.ts
@@ -1,61 +1,44 @@
-import { isPropertyOf, FieldKey, sortKeys, toArray } from '../../utils'
-import { SCHEMA_ERRORS } from './types'
+import { isPropertyOf, FieldKey, toArray } from '../../utils';
+import { SCHEMA_ERRORS } from './types';
 
-export { SchemaErrorTool, SchemaError }
+export { SchemaErrorTool, SchemaError };
 
 class SchemaError extends Error {
   constructor(public payload: ErrorPayload) {
-    super(SCHEMA_ERRORS.INVALID_SCHEMA)
+    super(SCHEMA_ERRORS.INVALID_SCHEMA);
   }
 }
 
 class SchemaErrorTool {
-  private _payload: ErrorPayload = {}
+  private _payload: ErrorPayload = {};
 
-  constructor(payload: ErrorPayload = {}) {
-    this._setPayload(payload)
-  }
+  constructor() {}
 
   get isPayloadLoaded() {
-    return Object.keys(this._payload).length > 0
+    return Object.keys(this._payload).length > 0;
   }
 
-  get data() {
-    return {
-      message: SCHEMA_ERRORS.INVALID_SCHEMA,
-      payload: sortKeys(this._payload)
-    }
-  }
-
-  private _has = (field: FieldKey) => isPropertyOf(field, this._payload)
-
-  private _setPayload(payload: InputPayload) {
-    Object.entries(payload).forEach(([key, value]) => {
-      this.add(key, value)
-    })
-  }
+  private _has = (field: FieldKey) => isPropertyOf(field, this._payload);
 
   add(field: FieldKey, value?: string | string[]) {
-    if (!value) value = []
-    else value = toArray(value)
+    value = toArray(value ?? []);
 
     if (this._has(field)) {
-      const currentValues = this._payload[field]
+      const currentValues = this._payload[field];
 
       value.forEach((v) => {
-        if (!currentValues.includes(v)) currentValues.push(v)
-      })
+        if (!currentValues.includes(v)) currentValues.push(v);
+      });
 
-      this._payload[field] = currentValues
-    } else this._payload[field] = value
+      this._payload[field] = currentValues;
+    } else this._payload[field] = value;
 
-    return this
+    return this;
   }
 
   throw() {
-    throw new SchemaError(this._payload)
+    throw new SchemaError(this._payload);
   }
 }
 
-type ErrorPayload = Record<FieldKey, string[]>
-type InputPayload = Record<FieldKey, string | string[]>
+type ErrorPayload = Record<FieldKey, string[]>;
