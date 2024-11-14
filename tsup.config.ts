b13d58714924d6b28a62a983bf1e03378aa601bb
kamtoeddy <kamtoeddytagne@gmail.com>
2024-11-14 14:59:55 +0100

    chore: update tsup.config.ts file so as to reduce bundle size

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from "tsup";
 
-export default defineConfig({
-  entry: ["./src/index.ts"],
-  format: ["esm", "cjs"],
-  outDir: "./dist",
-  clean: true,
-  dts: true,
-});
+export default defineConfig([
+  {
+    entry: ["./src/index.ts"],
+    format: "esm",
+    outDir: "./dist",
+    clean: true,
+    dts: true,
+  },
+  {
+    entry: ["./src/index.ts"],
+    format: ["esm", "cjs"],
+    outDir: "./dist",
+    clean: true,
+    dts: false,
+  },
+]);
