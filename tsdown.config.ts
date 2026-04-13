6305d35982a9fcc4c448ee9a22b8f7debfb73096
kamtoeddy <kamtoeddytagne@gmail.com>
2026-04-13 15:22:41 +0100

    chore: update deps

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,18 +1,18 @@
-import { defineConfig } from "tsdown";
+import { defineConfig } from 'tsdown';
 
 export default defineConfig([
   {
-    entry: ["./src/index.ts"],
-    format: ["esm", "cjs"],
-    outDir: "./dist",
+    entry: ['./src/index.ts'],
+    format: ['esm', 'cjs'],
+    outDir: './dist',
     clean: true,
     dts: false,
     minify: true,
   },
   {
-    entry: ["./src/index.ts"],
-    format: "esm",
-    outDir: "./dist",
+    entry: ['./src/index.ts'],
+    format: 'esm',
+    outDir: './dist',
     clean: true,
     dts: true,
     minify: true,
