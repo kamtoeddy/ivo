edafd11bcbed54d516bb6589e700b2b1abd74a72
kamtoeddy <kamtoeddytagne@gmail.com>
2026-04-13 15:18:37 +0100

    chore: migrate from tsup to tsdown

---
+++ tsdown.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "tsdown";
+
+export default defineConfig([
+  {
+    entry: ["./src/index.ts"],
+    format: ["esm", "cjs"],
+    outDir: "./dist",
+    clean: true,
+    dts: false,
+    minify: true,
+  },
+  {
+    entry: ["./src/index.ts"],
+    format: "esm",
+    outDir: "./dist",
+    clean: true,
+    dts: true,
+    minify: true,
+  },
+]);
