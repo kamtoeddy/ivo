b359330b88febf96e70a4542cd116cb929811647
kamtoeddy <kamtoeddytagne@gmail.com>
2026-05-03 10:06:22 +0100

    feat: add support for cjs

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,14 +1,24 @@
 import { defineConfig } from 'tsdown';
 
-export default defineConfig({
-  entry: ['./src/index.ts'],
-  format: {
-    esm: {
-      outExtensions: () => ({ dts: '.d.ts', js: '.js' }),
-    },
+export default defineConfig([
+  {
+    entry: ['./src/index.ts'],
+    format: 'cjs',
+    outDir: './dist',
+    clean: true,
+    dts: false,
+    minify: true,
   },
-  outDir: './dist',
-  clean: true,
-  dts: true,
-  minify: true,
-});
+  {
+    entry: ['./src/index.ts'],
+    format: {
+      esm: {
+        outExtensions: () => ({ dts: '.d.ts', js: '.js' }),
+      },
+    },
+    outDir: './dist',
+    clean: true,
+    dts: true,
+    minify: true,
+  },
+]);
