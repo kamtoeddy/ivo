2c39d60a012ab386732b74ad374a87932df93b17
kamtoeddy <kamtoeddytagne@gmail.com>
2026-06-05 14:03:34 +0100

    test: test demos against various async runtimes

--- rust/demos/src/main.rs
+++ rust/demos/src/main.rs
@@ -1,6 +1,17 @@
 use demos::users::run_users_demo;
 
-#[tokio::main]
+// #[tokio::main]
+#[async_std::main]
 async fn main() {
     run_users_demo().await
 }
+
+// use smol::{Unblock, io, net, prelude::*};
+
+// fn main() -> io::Result<()> {
+//     smol::block_on(async {
+//         run_users_demo().await;
+
+//         Ok(())
+//     })
+// }
