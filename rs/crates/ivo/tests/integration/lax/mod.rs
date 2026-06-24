41b78fb079e288bf2554fb91d2794a728603ea41
kamtoeddy <kamtoeddytagne@gmail.com>
2026-06-24 17:54:40 +0100

    test: add tests to make sure validation & re-validation of lax fields is properly handled at creation and during updates

--- rs/crates/ivo/tests/integration/lax/mod.rs
+++ rs/crates/ivo/tests/integration/lax/mod.rs
@@ -1,7 +1,7 @@
 #![cfg(test)]
 
-use ivo::{IvoField, IvoStruct, Schema};
-use std::{future::ready, panic};
+use ivo::{IvoField, IvoStruct, Schema, UpdateError};
+use std::{future::ready, ops::RangeInclusive, panic};
 
 use crate::async_test_matrix;
 
@@ -19,13 +19,13 @@ mod on_success;
 // [ ] readonly
 // [ ] required_if
 // [x] validate
-// [ ] re_validate
+// [x] re_validate
 // [ ] post_validate
 // [x] on_delete
 // [x] on_failure
 // [x] on_success
 
-async fn should_not_create_if_primary_validation_fails() {
+async fn should_not_create_if_any_primary_validation_fails() {
     #[derive(Debug, Clone, PartialEq, IvoStruct)]
     struct Data {
         lax: String,
@@ -84,11 +84,31 @@ async fn should_not_create_if_primary_validation_fails() {
             _ => unreachable!(),
         }
     }
+
+    let lax_values = [String::from("1".repeat(2)), String::from("1".repeat(3))];
+
+    for lax_value in lax_values {
+        let r = model
+            .create(
+                &PartialDataInput {
+                    lax: Some(lax_value.clone()),
+                },
+                None,
+            )
+            .await;
+
+        match r {
+            Ok((data, _)) => {
+                assert_eq!(data.lax, lax_value);
+            }
+            _ => unreachable!(),
+        }
+    }
 }
 
-async_test_matrix!(should_not_create_if_primary_validation_fails);
+async_test_matrix!(should_not_create_if_any_primary_validation_fails);
 
-async fn should_create_properly() {
+async fn should_not_create_if_secondary_any_validation_fails() {
     #[derive(Debug, Clone, PartialEq, IvoStruct)]
     struct Data {
         lax: String,
@@ -98,6 +118,9 @@ async fn should_create_properly() {
     struct DataInput {
         lax: String,
     }
+
+    const MIN_LENGTH_ERROR: &str = "expected lax to be at least 2 characters long";
+    const MIN_REVALIDATION_LENGTH_ERROR: &str = "expected lax to be at least 4 characters long";
 
     let schema: Schema<DataInput, Data> = Schema::new(
         |f| {
@@ -105,7 +128,24 @@ async fn should_create_properly() {
                 "lax",
                 IvoField::LAX
                     .default("default_value".into())
-                    .validate(|v: String, _, _| ready(Ok(v))),
+                    .validate(|v: String, _, _| {
+                        let validated = v.trim();
+
+                        if validated.len() < 2 {
+                            return ready(Err((MIN_LENGTH_ERROR.into(), None)));
+                        }
+
+                        ready(Ok(v))
+                    })
+                    .re_validate(|v: String, _, _| {
+                        let validated = v.trim();
+
+                        if validated.len() < 4 {
+                            return ready(Err((MIN_REVALIDATION_LENGTH_ERROR.into(), None)));
+                        }
+
+                        ready(Ok(v))
+                    }),
             )
         },
         |o| o,
@@ -113,47 +153,84 @@ async fn should_create_properly() {
 
     let model = schema.get_model();
 
-    let lax_value = String::from("value");
+    let lax_values = [
+        String::from(" 111"),
+        String::from(" 11 "),
+        String::from("11"),
+        String::from(" 112   "),
+    ];
 
-    let r = model
-        .create(
-            &PartialDataInput {
-                lax: Some(lax_value.clone()),
-            },
-            None,
-        )
-        .await;
+    for lax_value in lax_values {
+        let r = model
+            .create(
+                &PartialDataInput {
+                    lax: Some(lax_value),
+                },
+                None,
+            )
+            .await;
 
-    match r {
-        Ok((d, _)) => {
-            assert_eq!(d, Data { lax: lax_value })
+        match r {
+            Err((p, _)) => {
+                assert_eq!(
+                    p.get("lax").unwrap()[0].reason,
+                    MIN_REVALIDATION_LENGTH_ERROR
+                );
+            }
+            _ => unreachable!(),
         }
-        _ => unreachable!(),
+    }
+
+    let lax_values = [String::from("1".repeat(4)), String::from("1".repeat(5))];
+
+    for lax_value in lax_values {
+        let r = model
+            .create(
+                &PartialDataInput {
+                    lax: Some(lax_value.clone()),
+                },
+                None,
+            )
+            .await;
+
+        match r {
+            Ok((data, _)) => {
+                assert_eq!(data.lax, lax_value);
+            }
+            _ => unreachable!(),
+        }
     }
 }
 
-async_test_matrix!(should_create_properly);
+async_test_matrix!(should_not_create_if_secondary_any_validation_fails);
 
-async fn should_update_properly() {
+async fn should_not_update_if_any_primary_validation_fails() {
     #[derive(Debug, Clone, PartialEq, IvoStruct)]
     struct Data {
         id: i32,
-        lax: String,
+        lax: i32,
     }
 
     #[derive(Debug, Clone, IvoStruct)]
     struct DataInput {
-        lax: String,
+        lax: i32,
     }
+
+    const LAX_OUT_OF_RANGE_ERROR: &str = "lax must be between 1 & 5 inclussive";
+    const LAX_VALUE_RANGE: RangeInclusive<i32> = 1..=5;
 
     let schema: Schema<DataInput, Data> = Schema::new(
         |f| {
             f.set("id", IvoField::CONSTANT.computed(|_, _| ready(1)))
                 .set(
                     "lax",
-                    IvoField::LAX
-                        .default("default_value".into())
-                        .validate(|v: String, _, _| ready(Ok(v))),
+                    IvoField::LAX.default(1).validate(|v: i32, _, _| {
+                        if !LAX_VALUE_RANGE.contains(&v) {
+                            return ready(Err((LAX_OUT_OF_RANGE_ERROR.into(), None)));
+                        }
+
+                        ready(Ok(v))
+                    }),
                 )
         },
         |o| o,
@@ -161,35 +238,174 @@ async fn should_update_properly() {
 
     let model = schema.get_model();
 
-    let data = Data {
-        id: 1,
-        lax: String::from("value"),
-    };
+    let data = Data { id: 1, lax: 2 };
 
-    let updated_value = String::from("updated value");
+    let lax_values = [-1, 0, LAX_VALUE_RANGE.max().unwrap() + 1];
 
-    let r = model
-        .update(
-            &data,
-            &PartialDataInput {
-                lax: Some(updated_value.clone()),
-            },
-            None,
-        )
-        .await;
-
-    match r {
-        Ok((d, _)) => {
-            assert_eq!(
-                d,
-                PartialData {
-                    id: None,
-                    lax: Some(updated_value.clone()),
-                }
+    for lax_value in lax_values {
+        let r = model
+            .update(
+                &data,
+                &PartialDataInput {
+                    lax: Some(lax_value),
+                },
+                None,
             )
+            .await;
+
+        match r {
+            Err((e, _)) => match e {
+                UpdateError::ValidationError(p) => {
+                    assert_eq!(p.get("lax").unwrap()[0].reason, LAX_OUT_OF_RANGE_ERROR);
+                }
+                _ => unreachable!(),
+            },
+            _ => unreachable!(),
         }
-        _ => unreachable!(),
+    }
+
+    for updated_value in LAX_VALUE_RANGE.clone() {
+        if updated_value == data.lax {
+            continue;
+        }
+
+        let r = model
+            .update(
+                &data,
+                &PartialDataInput {
+                    lax: Some(updated_value),
+                },
+                None,
+            )
+            .await;
+
+        match r {
+            Ok((d, _)) => {
+                assert_eq!(
+                    d,
+                    PartialData {
+                        id: None,
+                        lax: Some(updated_value),
+                    }
+                )
+            }
+            _ => unreachable!(),
+        }
     }
 }
 
-async_test_matrix!(should_update_properly);
+async_test_matrix!(should_not_update_if_any_primary_validation_fails);
+
+async fn should_not_update_if_any_secondary_validation_fails() {
+    #[derive(Debug, Clone, PartialEq, IvoStruct)]
+    struct Data {
+        id: i32,
+        lax: i32,
+    }
+
+    #[derive(Debug, Clone, IvoStruct)]
+    struct DataInput {
+        lax: i32,
+    }
+
+    const LAX_OUT_OF_RANGE_ERROR: &str = "lax must be between 1 & 50 inclussive";
+    const LAX_VALUE_RANGE: RangeInclusive<i32> = 1..=50;
+
+    const REVALIDATED_LAX_OUT_OF_RANGE_ERROR: &str =
+        "revalidated lax must be between 10 & 5 inclussive";
+    const REVALIDATED_LAX_VALUE_RANGE: RangeInclusive<i32> = 10..=35;
+
+    let schema: Schema<DataInput, Data> = Schema::new(
+        |f| {
+            f.set("id", IvoField::CONSTANT.computed(|_, _| ready(1)))
+                .set(
+                    "lax",
+                    IvoField::LAX
+                        .default(1)
+                        .validate(|v: i32, _, _| {
+                            if !LAX_VALUE_RANGE.contains(&v) {
+                                return ready(Err((LAX_OUT_OF_RANGE_ERROR.into(), None)));
+                            }
+
+                            ready(Ok(v))
+                        })
+                        .re_validate(|v: i32, _, _| {
+                            if !REVALIDATED_LAX_VALUE_RANGE.contains(&v) {
+                                return ready(Err((
+                                    REVALIDATED_LAX_OUT_OF_RANGE_ERROR.into(),
+                                    None,
+                                )));
+                            }
+
+                            ready(Ok(v))
+                        }),
+                )
+        },
+        |o| o,
+    );
+
+    let model = schema.get_model();
+
+    let data = Data { id: 1, lax: 20 };
+
+    let lax_values = [
+        REVALIDATED_LAX_VALUE_RANGE.min().unwrap() - 1,
+        REVALIDATED_LAX_VALUE_RANGE.max().unwrap() + 1,
+    ];
+
+    for lax_value in lax_values {
+        let r = model
+            .update(
+                &data,
+                &PartialDataInput {
+                    lax: Some(lax_value),
+                },
+                None,
+            )
+            .await;
+
+        match r {
+            Err((e, _)) => match e {
+                UpdateError::ValidationError(p) => {
+                    assert_eq!(
+                        p.get("lax").unwrap()[0].reason,
+                        REVALIDATED_LAX_OUT_OF_RANGE_ERROR
+                    );
+                }
+                _ => unreachable!(),
+            },
+            _ => unreachable!(),
+        }
+    }
+
+    for updated_value in REVALIDATED_LAX_VALUE_RANGE.clone() {
+        if updated_value == data.lax {
+            continue;
+        }
+
+        let r = model
+            .update(
+                &data,
+                &PartialDataInput {
+                    lax: Some(updated_value),
+                },
+                None,
+            )
+            .await;
+
+        match r {
+            Ok((d, _)) => {
+                assert_eq!(
+                    d,
+                    PartialData {
+                        id: None,
+                        lax: Some(updated_value),
+                    }
+                )
+            }
+            _ => unreachable!(),
+        }
+    }
+}
+
+async_test_matrix!(should_not_update_if_any_secondary_validation_fails);
