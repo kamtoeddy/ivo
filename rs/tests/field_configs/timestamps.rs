6f201402aeaee37d4411c074b9857063016d87fe
kamtoeddy <kamtoeddytagne@gmail.com>
2026-07-02 10:31:33 +0100

    test: add tests to make sure schemas with either created_at or updated_at timestamps present on their input structs get rejected

--- rs/tests/field_configs/timestamps.rs
+++ rs/tests/field_configs/timestamps.rs
@@ -53,7 +53,60 @@ fn should_reject_if_created_at_is_enabled_with_custom_name_but_missing_from_outp
 }
 
 #[test]
-fn should_allow_if_created_at_is_enabled_with_default_name_and_is_on_output() {
+#[should_panic(
+    expected = "[created_at]: is a purely output field. It should not be present on \u{1b}[1mDataInput"
+)]
+fn should_reject_if_created_at_is_enabled_with_default_name_and_is_provided_on_input_struct() {
+    #[derive(Debug, Clone, PartialEq, IvoStruct)]
+    struct Data {
+        id: i32,
+        created_at: String,
+    }
+
+    #[derive(Debug, Clone, PartialEq, IvoStruct)]
+    struct DataInput {
+        created_at: String,
+    }
+
+    let _: Schema<DataInput, Data, Option<()>, &'static str, DefaultErrorTool> = Schema::new(
+        |f| {
+            f.set("id", IvoField::CONSTANT.computed(|_, _| ready(1234)))
+                .timestamps(|t| t.resolve(|| "Date.now()").created_at(None))
+        },
+        |o| o,
+    );
+}
+
+#[test]
+#[should_panic(
+    expected = "[custom_created_at]: is a purely output field. It should not be present on \u{1b}[1mDataInput"
+)]
+fn should_reject_if_created_at_is_enabled_with_custom_name_and_is_provided_on_input_struct() {
+    #[derive(Debug, Clone, PartialEq, IvoStruct)]
+    struct Data {
+        id: i32,
+        custom_created_at: String,
+    }
+
+    #[derive(Debug, Clone, PartialEq, IvoStruct)]
+    struct DataInput {
+        custom_created_at: String,
+    }
+
+    let _: Schema<DataInput, Data, Option<()>, &'static str, DefaultErrorTool> = Schema::new(
+        |f| {
+            f.set("id", IvoField::CONSTANT.computed(|_, _| ready(1234)))
+                .timestamps(|t| {
+                    t.resolve(|| "Date.now()")
+                        .created_at(Some("custom_created_at"))
+                })
+        },
+        |o| o,
+    );
+}
+
+#[test]
+fn should_allow_if_created_at_is_enabled_with_default_name_and_is_on_output_struct() {
     #[derive(Debug, Clone, PartialEq, IvoStruct)]
     struct Data {
         id: i32,
@@ -79,7 +132,7 @@ fn should_allow_if_created_at_is_enabled_with_default_name_and_is_on_output() {
 }
 
 #[test]
-fn should_allow_if_created_at_is_enabled_with_custom_name_band_is_onoutput() {
+fn should_allow_if_created_at_is_enabled_with_custom_name_band_is_on_output_struct() {
     #[derive(Debug, Clone, PartialEq, IvoStruct)]
     struct Data {
         id: i32,
@@ -159,7 +212,115 @@ fn should_reject_if_updated_at_is_enabled_with_custom_name_but_missing_from_outp
 }
 
 #[test]
-fn should_allow_if_updated_at_is_enabled_with_default_name_and_is_on_output() {
+#[should_panic(
+    expected = "[updated_at]: is a purely output field. It should not be present on \u{1b}[1mDataInput"
+)]
+fn should_reject_if_updated_at_is_enabled_with_default_name_and_is_provided_on_input_struct() {
+    #[derive(Debug, Clone, PartialEq, IvoStruct)]
+    struct Data {
+        id: i32,
+        updated_at: String,
+    }
+
+    #[derive(Debug, Clone, PartialEq, IvoStruct)]
+    struct DataInput {
+        updated_at: String,
+    }
+
+    let _: Schema<DataInput, Data, Option<()>, &'static str, DefaultErrorTool> = Schema::new(
+        |f| {
+            f.set("id", IvoField::CONSTANT.computed(|_, _| ready(1234)))
+                .timestamps(|t| t.resolve(|| "Date.now()").updated_at(None))
+        },
+        |o| o,
+    );
+}
+
+#[test]
+#[should_panic(
+    expected = "[custom_updated_at]: is a purely output field. It should not be present on \u{1b}[1mDataInput"
+)]
+fn should_reject_if_updated_at_is_enabled_with_custom_name_and_is_provided_on_input_struct() {
+    #[derive(Debug, Clone, PartialEq, IvoStruct)]
+    struct Data {
+        id: i32,
+        custom_updated_at: String,
+    }
+
+    #[derive(Debug, Clone, PartialEq, IvoStruct)]
+    struct DataInput {
+        custom_updated_at: String,
+    }
+
+    let _: Schema<DataInput, Data, Option<()>, &'static str, DefaultErrorTool> = Schema::new(
+        |f| {
+            f.set("id", IvoField::CONSTANT.computed(|_, _| ready(1234)))
+                .timestamps(|t| {
+                    t.resolve(|| "Date.now()")
+                        .updated_at(Some("custom_updated_at"))
+                })
+        },
+        |o| o,
+    );
+}
+
+#[test]
+#[should_panic(
+    expected = "[updated_at]: is a purely output field. It should not be present on \u{1b}[1mDataInput"
+)]
+fn should_reject_if_optional_updated_at_is_enabled_with_default_name_and_is_provided_on_input_struct(
+) {
+    #[derive(Debug, Clone, PartialEq, IvoStruct)]
+    struct Data {
+        id: i32,
+        updated_at: String,
+    }
+
+    #[derive(Debug, Clone, PartialEq, IvoStruct)]
+    struct DataInput {
+        updated_at: String,
+    }
+
+    let _: Schema<DataInput, Data, Option<()>, &'static str, DefaultErrorTool> = Schema::new(
+        |f| {
+            f.set("id", IvoField::CONSTANT.computed(|_, _| ready(1234)))
+                .timestamps(|t| t.resolve(|| "Date.now()").optional_updated_at(None))
+        },
+        |o| o,
+    );
+}
+
+#[test]
+#[should_panic(
+    expected = "[custom_updated_at]: is a purely output field. It should not be present on \u{1b}[1mDataInput"
+)]
+fn should_reject_if_optional_updated_at_is_enabled_with_custom_name_and_is_provided_on_input_struct(
+) {
+    #[derive(Debug, Clone, PartialEq, IvoStruct)]
+    struct Data {
+        id: i32,
+        custom_updated_at: String,
+    }
+
+    #[derive(Debug, Clone, PartialEq, IvoStruct)]
+    struct DataInput {
+        custom_updated_at: String,
+    }
+
+    let _: Schema<DataInput, Data, Option<()>, &'static str, DefaultErrorTool> = Schema::new(
+        |f| {
+            f.set("id", IvoField::CONSTANT.computed(|_, _| ready(1234)))
+                .timestamps(|t| {
+                    t.resolve(|| "Date.now()")
+                        .optional_updated_at(Some("custom_updated_at"))
+                })
+        },
+        |o| o,
+    );
+}
+
+#[test]
+fn should_allow_if_updated_at_is_enabled_with_default_name_and_is_on_output_struct() {
     #[derive(Debug, Clone, PartialEq, IvoStruct)]
     struct Data {
         id: i32,
@@ -185,7 +346,7 @@ fn should_allow_if_updated_at_is_enabled_with_default_name_and_is_on_output() {
 }
 
 #[test]
-fn should_allow_if_updated_at_is_enabled_with_custom_name_and_is_on_output() {
+fn should_allow_if_updated_at_is_enabled_with_custom_name_and_is_on_output_struct() {
     #[derive(Debug, Clone, PartialEq, IvoStruct)]
     struct Data {
         id: i32,
