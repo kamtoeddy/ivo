77d259b383536149dcae406a12038977e93c14a1
kamtoeddy <kamtoeddytagne@gmail.com>
2026-07-07 11:14:03 +0100

    feat: complete logic and tests to make built-in credit card validator to work properly

--- rs/crates/validators/src/lib.rs
+++ rs/crates/validators/src/lib.rs
@@ -6,15 +6,28 @@ static EMAIL_RE: LazyLock<Regex> = LazyLock::new(|| {
     Regex::new(r#"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"#).unwrap()
 });
 
-type ValidatorResult<T, E = ()> = Result<T, E>;
+pub fn validate_email(value: &str) -> ValidatorResult<String, String> {
+    let validated = value.trim();
+
+    if EMAIL_RE.is_match(&validated) {
+        return Ok(validated.to_string());
+    }
+
+    Err("Invalid email".into())
+}
 
 pub fn validate_credit_card(value: &str) -> ValidatorResult<String, String> {
     let validated = value.trim();
     let error = "Invalid card number".into();
+    const EXPECTED_LENGTH: usize = 16;
+
+    if validated.len() != EXPECTED_LENGTH {
+        return Err(error);
+    }
 
     let digits: Vec<u32> = validated.chars().filter_map(|c| c.to_digit(10)).collect();
 
-    if digits.len() != 16 {
+    if digits.len() != EXPECTED_LENGTH {
         return Err(error);
     }
 
@@ -23,7 +36,16 @@ pub fn validate_credit_card(value: &str) -> ValidatorResult<String, String> {
         .iter()
         .take(15)
         .enumerate()
-        .map(|(i, &d)| if i % 2 == 0 { d * 2 } else { d })
+        .map(|(i, &d)| {
+            let value = if i % 2 == 0 { d * 2 } else { d };
+
+            // we want to get the sum of single digits
+            // we move from 12 to 1 + 2 == (3)
+            let first = value / 10;
+            let second = value % 10;
+
+            first + second
+        })
         .collect();
 
     let sum: u32 = to_check.iter().sum();
@@ -35,12 +57,4 @@ pub fn validate_credit_card(value: &str) -> ValidatorResult<String, String> {
     Ok(digits.iter().map(|n| n.to_string()).collect::<String>())
 }
 
-pub fn validate_email(value: &str) -> ValidatorResult<String, String> {
-    let validated = value.trim();
-
-    if EMAIL_RE.is_match(&validated) {
-        return Ok(validated.to_string());
-    }
-
-    Err("Invalid email".into())
-}
+type ValidatorResult<T, E = ()> = Result<T, E>;
--- rs/crates/validators/tests/mod.rs
+++ rs/crates/validators/tests/mod.rs
@@ -1,4 +1,4 @@
-use ivo_validators::validate_email;
+use ivo_validators::{validate_credit_card, validate_email};
 
 #[test]
 fn test_email() {
@@ -13,28 +13,28 @@ fn test_email() {
     }
 }
 
-// #[test]
-// fn test_validate_credit_card_truthy_values() {
-//     let truthy_values = [
-//         ("5420596721435293", "5420596721435293"),
-//         ("5420596721435293 ", "5420596721435293"),
-//         (" 5420596721435293 ", "5420596721435293"),
-//     ];
+#[test]
+fn test_validate_credit_card_truthy_values() {
+    let truthy_values = [
+        ("5420596721435293", "5420596721435293"),
+        ("5420596721435293 ", "5420596721435293"),
+        (" 5420596721435293 ", "5420596721435293"),
+    ];
 
-//     for (value, expected_validated) in truthy_values {
-//         let validated = validate_credit_card(value).ok().unwrap();
+    for (value, expected_validated) in truthy_values {
+        let validated = validate_credit_card(value).ok().unwrap();
 
-//         assert_eq!(validated, expected_validated);
-//     }
-// }
+        assert_eq!(validated, expected_validated);
+    }
+}
 
-// #[test]
-// fn test_validate_credit_card_falsy_values() {
-//     let falsy_values = ["", "123-2342-25-6750", "4187622910505690"];
+#[test]
+fn test_validate_credit_card_falsy_values() {
+    let falsy_values = ["", "123-2342-25-6750", "4187622910505690"];
 
-//     for value in falsy_values {
-//         let reason = validate_credit_card(value).err().unwrap();
+    for value in falsy_values {
+        let reason = validate_credit_card(value).err().unwrap();
 
-//         assert_eq!(reason, "Invalid card number");
-//     }
-// }
+        assert_eq!(reason, "Invalid card number");
+    }
+}
