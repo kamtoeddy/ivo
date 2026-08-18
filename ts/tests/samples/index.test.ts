fd407fe0446b456ba0bcaa2871d54f99add73ec5
kamtoeddy <kamtoeddytagne@gmail.com>
2026-08-18 17:46:23 +0100

    test(ivo_ts): update some tests

--- ts/tests/samples/index.test.ts
+++
@@ -1,6 +0,0 @@
-import { OrderItem } from './orderItem';
-import { StoreItemModel } from './storeItem';
-import { CommonInheritanceTest } from './storeItem/common-tests';
-
-CommonInheritanceTest(StoreItemModel, 'StoreItemModel');
-CommonInheritanceTest(OrderItem, 'OrderItem');
---
+++ ts/tests/samples/orderItem/index.test.ts
@@ -0,0 +1,56 @@
+import { beforeAll, describe, expect, it } from 'bun:test';
+
+import { OrderItem } from '.';
+import type { IOrderItem } from './types';
+
+describe('Testing schema of Orderitem (inherited schema)', () => {
+  let orderItem: IOrderItem;
+
+  beforeAll(async () => {
+    orderItem = (
+      await OrderItem.create(
+        {
+          id: '1',
+          name: 'beer',
+          costPrice: 5,
+          price: 5,
+          measureUnit: 'bottle',
+          otherMeasureUnits: [
+            { coefficient: 24, name: 'crate24' },
+            { coefficient: 5, name: 'tray' },
+            { coefficient: 12, name: 'crate' },
+          ],
+          __quantity: 100,
+        },
+        {},
+      )
+    ).data!;
+  });
+
+  it('should have been created properly', () => {
+    expect(orderItem).toMatchObject({
+      id: '1',
+      name: 'beer',
+      costPrice: 5,
+      measureUnit: 'bottle',
+      otherMeasureUnits: [
+        { coefficient: 12, name: 'crate' },
+        { coefficient: 24, name: 'crate24' },
+        { coefficient: 5, name: 'tray' },
+      ],
+      price: 5,
+      quantity: 100,
+    });
+  });
+
+  it('should not have properties of parent removed during inheritance', () => {
+    const removedProps = [
+      '_readOnlyNoInit',
+      '_dependentReadOnly',
+      '_sideEffectForDependentReadOnly',
+    ];
+    const props = Object.keys(orderItem);
+
+    for (const prop of props) expect(removedProps.includes(prop)).toBe(false);
+  });
+});
