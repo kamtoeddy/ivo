1f1802e2b603a526f1ac670e8f00d2e265c054d2
kamtoeddy <kamtoeddytagne@gmail.com>
2026-09-01 00:21:03 +0100

    test: port more tests from the old impl

--- rs-next/tests/field_configs/compile_fail/dependents.rs
+++ rs-next/tests/field_configs/compile_fail/dependents.rs
@@ -33,4 +33,90 @@ mod bare_ident_ivo_virtual_alias {
     }
 }
 
+#[ivo_schema(
+    input(DataInput, derive(Debug, Clone, PartialEq)),
+    output(Data, derive(Debug, Clone, PartialEq))
+)]
+mod empty_parents {
+    struct Fields {
+        #[required]
+        pub name: String,
+
+        #[depends_on()]
+        #[default(String::new())]
+        #[resolve(|_, _| String::new())]
+        pub dependent: String,
+    }
+}
+
+#[ivo_schema(
+    input(DataInput, derive(Debug, Clone, PartialEq)),
+    output(Data, derive(Debug, Clone, PartialEq))
+)]
+mod self_dependency {
+    struct Fields {
+        #[required]
+        pub name: String,
+
+        #[depends_on("dependent")]
+        #[default(String::new())]
+        #[resolve(|_, _| String::new())]
+        pub dependent: String,
+    }
+}
+
+#[ivo_schema(
+    input(DataInput, derive(Debug, Clone, PartialEq)),
+    output(Data, derive(Debug, Clone, PartialEq))
+)]
+mod unknown_parent {
+    struct Fields {
+        #[required]
+        pub name: String,
+
+        #[depends_on("ghost")]
+        #[default(String::new())]
+        #[resolve(|_, _| String::new())]
+        pub dependent: String,
+    }
+}
+
+#[ivo_schema(
+    input(DataInput, derive(Debug, Clone, PartialEq)),
+    output(Data, derive(Debug, Clone, PartialEq))
+)]
+mod duplicate_parent {
+    struct Fields {
+        #[required]
+        pub name: String,
+
+        #[depends_on("name", "name")]
+        #[default(String::new())]
+        #[resolve(|_, _| String::new())]
+        pub dependent: String,
+    }
+}
+
+#[ivo_schema(
+    input(DataInput, derive(Debug, Clone, PartialEq)),
+    output(Data, derive(Debug, Clone, PartialEq))
+)]
+mod dependency_on_timestamp {
+    struct Fields {
+        #[required]
+        pub name: String,
+
+        #[created_at]
+        pub created_at: String,
+
+        #[depends_on("created_at")]
+        #[default(String::new())]
+        #[resolve(|_, _| String::new())]
+        pub dependent: String,
+    }
+
+    #[timestamps(|| String::from("timestamp"))]
+    const _: () = ();
+}
+
 fn main() {}
